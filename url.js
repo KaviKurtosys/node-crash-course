676e4c2a63a12fdcce90cd995dcc539dae2e04a1
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-11 09:35:05 +0200

    Logging paths

--- url.js
+++ url.js
@@ -5,3 +5,4 @@ const address = 'http://localhost:8080/default.html?year=2017&month=february';
 const parsedUrl = url.parse(address, true);
 
 console.log(parsedUrl.host);
+console.log(parsedUrl.pathname);
